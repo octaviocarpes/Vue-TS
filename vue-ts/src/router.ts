3e18d22330442f5769126ac58dc7fb6149e96e1e
octaviocarpes <octavio.carpes10@gmail.com>
2019-02-20 13:34:15 -0300

    Adding first component

--- vue-ts/src/router.ts
+++ vue-ts/src/router.ts
@@ -11,16 +11,17 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () => import("@/views/Home.vue")
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: () => import("./views/About.vue")
+    },
+    {
+      path: "/pokemons",
+      name: "pokemons",
+      component: () => import("./views/pokemons/Pokemons.vue")
     }
   ]
 });
---
+++ vue-ts/src/views/pokemons/Pokemons.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  .container
+    h1 Pokémons
+</template>
+
+<script lang="ts" src="./app.ts"></script>
+
+<style lang="scss" scoped>
+@import url("./styles.scss");
+</style>
